618fdb99540962c9a2dc1fea2dfc7b72501d8d68
Feng Shu <tumashu@163.com>
2020-02-17 11:17:35 +0800

    refactor code

--- share2computer.el
+++ share2computer.el
@@ -51,16 +51,37 @@
   :group 'org-brain
   :type '(string))
 
-(defvar share2computer-file-number 0
+(defvar share2computer--file-number 0
   "The number of downloaded files.")
-(defvar share2computer-buffers nil
+(defvar share2computer--buffers nil
   "The buffers created by `url-retrieve' when run share2computer.")
-(defvar share2computer-timer1 nil
+(defvar share2computer--timer1 nil
   "The timer used to cancel download.")
-(defvar share2computer-timer2 nil
+(defvar share2computer--timer2 nil
   "The timer used to show second of time when connect url.")
 
-(defun share2computer-write (status url link path n &optional retry-n)
+(defun share2computer--register (url buffer)
+  (push buffer (alist-get url share2computer--buffers nil t 'equal)))
+
+(defun share2computer--registered-p (url buffer)
+  (member buffer (alist-get url share2computer--buffers nil t 'equal)))
+
+(defun share2computer--kill (url &optional reverse)
+  (let ((kill-buffer-query-functions nil)
+        buffers result)
+    (dolist (x share2computer--buffers)
+      (let ((condi (or (equal url (car x))
+                       (equal url 'all))))
+        (if (if reverse (not condi) condi)
+            (dolist (buff (cdr x))
+              (when (buffer-live-p buff)
+                (push buff buffers)))
+          (push x result)))
+      (setq share2computer--buffers result)
+      ;; 必须先设置 share2computer--buffers 然后再删除 buffer
+      (mapc #'kill-buffer buffers))))
+
+(defun share2computer--write (status url link path n &optional retry-n)
   (let* ((err (plist-get status :error))
          (disposition
           (mail-fetch-field "Content-Disposition"))
@@ -80,42 +101,17 @@
              (point)))
           (let ((coding-system-for-write 'no-conversion))
             (write-region nil nil file))
-          (setq share2computer-file-number
-                (+ share2computer-file-number 1))
-          (if (= share2computer-file-number n)
+          (setq share2computer--file-number
+                (+ share2computer--file-number 1))
+          (if (= share2computer--file-number n)
               (progn
                 (message "share2computer: download finished from %S" url)
                 (eh-system-open path))
             (message "share2computer: download %s/%s files to %S ..."
-                     share2computer-file-number n path)))
-      (share2computer-download-1 (current-buffer) url link path n 1))))
+                     share2computer--file-number n path)))
+      (share2computer--download-1 (current-buffer) url link path n 1))))
 
-(defun share2computer-kill-all ()
-  (interactive)
-  (share2computer-kill 'all))
-
-(defun share2computer-kill (url &optional reverse)
-  (let ((kill-buffer-query-functions nil)
-        buffers result)
-    (dolist (x share2computer-buffers)
-      (let ((condi (or (equal url (car x))
-                       (equal url 'all))))
-        (if (if reverse (not condi) condi)
-            (dolist (buff (cdr x))
-              (when (buffer-live-p buff)
-                (push buff buffers)))
-          (push x result)))
-      (setq share2computer-buffers result)
-      ;; 必须先设置 share2computer-buffers 然后再删除 buffer
-      (mapcar #'kill-buffer buffers))))
-
-(defun share2computer-register (url buffer)
-  (push buffer (alist-get url share2computer-buffers nil t 'equal)))
-
-(defun share2computer-registered-p (url buffer)
-  (member buffer (alist-get url share2computer-buffers nil t 'equal)))
-
-(defun share2computer-download (status url path)
+(defun share2computer--download (status url path)
   (let ((n (save-excursion
              (goto-char (point-min))
              (re-search-forward "\n\n" nil 'move)
@@ -124,79 +120,90 @@
                            (json-read-from-string
                             (buffer-substring (point) (point-max)))))))))
     (when (and (numberp n) (> n 0))
-      (share2computer-kill url t)
-      (share2computer-cancel-timer)
+      (share2computer--kill url t)
+      (share2computer--cancel-timer)
       (message "share2computer: start download ...")
       (dotimes (i n)
-        (share2computer-download-1
+        (share2computer--download-1
          (current-buffer) url (format "%s%S" url i) path n)))))
 
-(defun share2computer-download-1 (buffer url link path n &optional retry-n)
+(defun share2computer--download-1 (buffer url link path n &optional retry-n)
   (if (and (numberp retry-n)
            (> retry-n 4))
       (message "share2computer: fail after retry download 3 times !!!")
-    (share2computer-register
+    (share2computer--register
      url
      (url-retrieve
       link
       (lambda (status buffer url link path n)
-        (when (share2computer-registered-p url buffer)
-          (share2computer-write status url link path n)))
+        (when (share2computer--registered-p url buffer)
+          (share2computer--write status url link path n)))
       (list buffer url link path n)
       t t))
     (when (numberp retry-n)
       (message "share2computer: retry(%s) download file from %S ..." retry-n link)
       (setq retry-n (+ retry-n 1)))))
 
-(defun share2computer-active-timer ()
+(defun share2computer--active-timers ()
   "Active timers which used to download cancel and progress."
   (let ((sec (string-to-number (format-time-string "%s"))))
-    (share2computer-cancel-timer)
-    (setq share2computer-timer1
+    (share2computer--cancel-timer)
+    (setq share2computer--timer1
           (run-with-timer
            4 nil
            (lambda ()
              (message "share2computer: cancel download for wait too long time.")
-             (share2computer-cancel-timer)
-             (share2computer-kill 'all))))
-    (setq share2computer-timer2
+             (share2computer--cancel-timer)
+             (share2computer--kill 'all))))
+    (setq share2computer--timer2
           (run-with-timer
            nil 1
            `(lambda ()
               (message "share2computer: read info (%ss) ..."
                        (- (string-to-number (format-time-string "%s")) ,sec)))))))
 
-(defun share2computer-cancel-timer ()
+(defun share2computer--cancel-timer ()
   "Cancel timers which used to download cancel and progress."
-  (when share2computer-timer1
-    (cancel-timer share2computer-timer1))
-  (when share2computer-timer2
-    (cancel-timer share2computer-timer2)))
+  (when share2computer--timer1
+    (cancel-timer share2computer--timer1))
+  (when share2computer--timer2
+    (cancel-timer share2computer--timer2)))
 
-(defun share2computer-internal (path)
+(defun share2computer--internal (path)
   "Internal function of share2computer. "
   (setq path (expand-file-name (file-name-as-directory path)))
-  (setq share2computer-file-number 0)
+  (setq share2computer--file-number 0)
   (make-directory path t)
   (while (not (cl-some (lambda (x)
                          (> (length x) 0))
                        share2computer-urls))
     (share2computer-setup))
-  (share2computer-kill 'all)
-  (share2computer-active-timer)
+  (share2computer--kill 'all)
+  (share2computer--active-timers)
   (dolist (url share2computer-urls)
     (let ((url (file-name-as-directory url)))
-      (share2computer-register
+      (share2computer--register
        url (url-retrieve (concat url "info")
-                         'share2computer-download
+                         'share2computer--download
                          (list url path)
                          t t)))))
 
+
+;;;###autoload
+(defun share2computer-kill ()
+  (interactive)
+  (share2computer--kill 'all))
+
+;;;###autoload
 (defun share2computer ()
   "Download files shared by Android ShareToComputer."
   (interactive)
-  (share2computer-internal share2computer-default-path))
+  (share2computer--internal share2computer-default-path))
 
+(declare-function org-back-to-heading "org" (&optional invisible-ok))
+(declare-function org-attach-dir "org" (&optional create-if-not-exists-p no-fs-check))
+
+;;;###autoload
 (defun share2computer-org ()
   "Download files shared by Android ShareToComputer to org attach dir."
   (interactive)
@@ -211,7 +218,7 @@
 	(set-buffer (marker-buffer marker))
 	(goto-char marker))
       (org-back-to-heading t)
-      (share2computer-internal (org-attach-dir t)))))
+      (share2computer--internal (org-attach-dir t)))))
 
 (defun share2computer-setup ()
   "Setup share2computer."
@@ -220,14 +227,13 @@
     (while status
       (push (read-from-minibuffer "share2computer url: " "http://192.168.0.X:8080")
             share2computer-urls)
-      (when (y-or-n-p "share2computer: adding url finish? ")
+      (unless (y-or-n-p "share2computer: adding another url? ")
         (setq status nil))))
   (setq share2computer-urls
         (delete-dups share2computer-urls))
-  (when (y-or-n-p "Save this url for future session? ")
-    (customize-save-variable
-     'share2computer-urls
-     share2computer-urls)))
+  (customize-save-variable
+   'share2computer-urls
+   share2computer-urls))
 
 ;; * Footer
 (provide 'share2computer)
